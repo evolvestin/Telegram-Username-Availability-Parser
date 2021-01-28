beeca92e06d2567630c05608f98fa765e68e5a8c
me <steve10.live@gmail.com>
2021-01-29 02:54:07 +0300

    Created master script

--- main.py
+++ main.py
@@ -1,183 +1,26 @@
 import os
-import re
-import gspread
+import stat
+import shutil
 import objects
-import _thread
-import requests
-from time import sleep
-from GDrive import Drive
-from copy import deepcopy
-from itertools import product
-from bs4 import BeautifulSoup
-from string import ascii_lowercase
+from git.repo.base import Repo
 # ========================================================================================================
-stamp1 = objects.time_now()
-folder_id = ''
+stamp = objects.time_now()
 
 
-def variables_creation():
-    global folder_id
-    db = {}
-    combs = []
-    user_postfix = ''
-    max_len_combs = 0
-    file_name = 'symbol'
-    prefix = str(min_length)
-    objects.environmental_files()
-    drive_client = Drive('google.json')
-    drive_client_files = drive_client.files()
-    if min_length != max_length:
-        prefix += '-' + str(max_length)
-    if os.environ.get('user_postfix'):
-        user_postfix = os.environ['user_postfix']
-        file_name += '+' + user_postfix
-
-    file_name = prefix + file_name
-    files = {file_name + postfix + '.txt': '' for postfix in ['_used', '_clear']}
-
-    for file in drive_client_files:
-        if file['name'] == 'telegram_usernames':
-            folder_id = file['id']
-        if file['name'] in list(files.keys()):
-            files[file['name']] = file['id']
-            drive_client.download_file(file['id'], file['name'])
-
-    for file in files:
-        if os.path.exists(file) is False:
-            with open(file, 'w') as f:
-                f.write('CLEAR_FILE_FILLER')
-                f.close()
-
-    for file in files:
-        opened = open(file, 'r')
-        text = re.sub('CLEAR_FILE_FILLER', '', opened.read())
-        if text:
-            db[file] = text.split(' ')
-        else:
-            db[file] = []
-        opened.close()
-
-    frame = (len(db[file_name + '_used.txt']) // 500000) * 500000
-    for length in range(min_length, max_length + 1):
-        for value in product(ascii_lowercase + '_', repeat=length):
-            username = ''.join(value) + user_postfix
-            if username.startswith('_') is False and username.endswith('_') is False:
-                if re.search('__+', username) is None:
-                    max_len_combs += 1
-                    if max_len_combs in range(frame, frame + 500000):
-                        combs.append(username)
-    print('len(array_db[' + file_name + '_used.txt]) =', len(db[file_name + '_used.txt']))
-    print('len(combs) =', len(combs))
-    return db, files, combs, file_name, max_len_combs
+def delete(action, name, exc):
+    os.chmod(name, stat.S_IWRITE)
+    os.remove(name)
+    return action, name, exc
 
 
-t_me = 'https://t.me/'
-min_length = os.environ.get('min_length')
-max_length = os.environ.get('max_length')
-Auth = objects.AuthCentre(os.environ['TOKEN'])
-ErrorAuth = objects.AuthCentre(os.environ['ERROR-TOKEN'])
-if min_length and max_length:
-    min_length, max_length = int(min_length), int(max_length)
-    array_db, file_names, combinations, main_file, max_len_combinations = variables_creation()
-    Auth.start_message(stamp1)
-else:
-    Auth.start_message(stamp1, '\nОшибка с переменными окружения.\n' + objects.bold('Бот выключен'))
-    array_db, file_names, combinations, main_file, max_len_combinations = {}, {}, [], '', 1
+Repo.clone_from('https://github.com/evolvestin/Telegram-Username-Availability-Parser/', 'temp')
+for file_name in os.listdir('temp/master'):
+    shutil.copy(f'temp/master/{file_name}', file_name)
+shutil.rmtree('temp', onerror=delete)
 # ========================================================================================================
-
-
-def checking():
-    global array_db
-    if combinations:
-        while True:
-            try:
-                for username in combinations:
-                    if username not in array_db[main_file + '_used.txt']:
-                        sleep(0.003)
-                        try:
-                            response = requests.get(t_me + username)
-                        except IndexError and Exception:
-                            sleep(0.01)
-                            try:
-                                response = requests.get(t_me + username)
-                            except IndexError and Exception:
-                                response = None
-                        if response:
-                            soup = BeautifulSoup(response.text, 'html.parser')
-                            is_username_exist = soup.find('a', class_='tgme_action_button_new')
-                            if is_username_exist is None:
-                                array_db[main_file + '_clear.txt'].append(username)
-                            array_db[main_file + '_used.txt'].append(username)
-            except IndexError and Exception:
-                ErrorAuth.thread_exec()
-
-
-def save_array_to_file(path, array):
-    file = open(path, 'w')
-    text = ' '.join(array)
-    if text == '':
-        text = 'CLEAR_FILE_FILLER'
-    file.write(text)
-    file.close()
-
-
-def files_upload():
-    global array_db, file_names
-    drive_client = Drive('google.json')
-    for file in file_names:
-        if file_names[file] == '':
-            response = drive_client.create_file(file, folder_id)
-            file_names[file] = response['id']
-            objects.printer(file + ' создан')
-    while True:
-        try:
-            sleep(20)
-            temp_db = deepcopy(array_db)
-            for file_name in file_names:
-                save_array_to_file(file_name, temp_db[file_name])
-                try:
-                    drive_client.update_file(file_names[file_name], file_name)
-                except IndexError and Exception:
-                    drive_client = Drive('google.json')
-                    drive_client.update_file(file_names[file_name], file_name)
-
-            if len(temp_db[main_file + '_used.txt']) == max_len_combinations:
-                log_text = 'Цикл проверок доступности юзеров пройден. Записываем список свободных username'
-                objects.printer(log_text + ' (' + main_file + ') в google')
-                title = main_file + '/' + str(objects.time_now())
-                file = open(main_file + '_clear.txt', 'r')
-                text = file.read()
-                file.close()
-                chunks = [text[offset: offset + 50000] for offset in range(0, len(text), 50000)]
-                spreadsheet = gspread.service_account('google.json').create(title, folder_id)
-                worksheet = spreadsheet.sheet1
-                worksheet.resize(rows=len(chunks), cols=1)
-                spreadsheet.batch_update(objects.properties_json(worksheet.id, len(chunks), chunks[:200]))
-                if len(chunks) > 200:
-                    request_counter = 1
-                    for loop in range(200, len(chunks), 200):
-                        row_begin = loop + 1
-                        row_end = row_begin + 200
-                        if row_end > len(chunks):
-                            row_end = len(chunks)
-                        work_range = worksheet.range('A' + str(row_begin) + ':A' + str(row_end))
-                        for row in range(row_begin, row_end + 1):
-                            if row <= len(chunks):
-                                work_range[row - loop - 1].value = chunks[row - 1]
-                        worksheet.update_cells(work_range)
-                        request_counter += 1
-                        if request_counter == 50:
-                            request_counter = 0
-                            sleep(100)
-                objects.printer('Успешно записана информация в google')
-                for file_name in array_db:
-                    array_db[file_name] = []
-                    save_array_to_file(file_name, array_db[file_name])
-                objects.printer('Запущен новый цикл проверок, файлы очищены')
-        except IndexError and Exception:
-            ErrorAuth.thread_exec()
+print(f'Запуск оболочки за {objects.time_now() - stamp} секунды')
 
 
 if __name__ == '__main__':
-    _thread.start_new_thread(files_upload, ())
-    checking()
+    from master import start
+    start(stamp)
---
+++ master/master.py
@@ -0,0 +1,229 @@
+import os
+import re
+import gspread
+import objects
+import heroku3
+import _thread
+from time import sleep
+from GDrive import Drive
+from itertools import product
+from string import ascii_lowercase, ascii_uppercase
+# ========================================================================================================
+
+
+def variables_creation():
+    global master
+    objects.environmental_files()
+    sheet = gspread.service_account('google.json').open('master').worksheet('main')
+    drive_client = Drive('google.json')
+    resources = sheet.get('A1:Z50000')
+    keys = resources.pop(0)
+    scripts = []
+    for row in resources:
+        if len(row) == len(keys):
+            worker = {'update': 0, 'server': '1'}
+            for i in range(0, len(keys)):
+                worker[keys[i]] = row[i]
+            scripts.append(worker)
+
+    for worker in scripts:
+        split = {'1': {}, '2': {}}
+        for key in worker:
+            if key[-1] in ['1', '2']:
+                split[key[-1]][key[:-1].strip()] = worker[key]
+        if os.environ.get('local') is None:
+            for key in split:
+                connection = heroku3.from_key(split[key]['HEROKU API'])
+                if connection.account().email != split[key]['EMAIL']:
+                    worker[f'EMAIL {key}'] = connection.account().email
+                    worker['update'] = 1
+                for app in connection.apps():
+                    if len(app.dynos()) > 0:
+                        worker['server'] = key
+                    if app.name != split[key]['THREAD']:
+                        worker[f'THREAD {key}'] = app.name
+                        worker['update'] = 1
+
+    for folder in drive_client.files(only_folders=True):
+        if folder['name'] == 'temp':
+            master['temp_id'] = folder['id']
+        elif folder['name'] == 'telegram_usernames':
+            master['main_id'] = folder['id']
+        elif folder['name'].startswith('logs'):
+            if master['logs_created_time'] <= folder['createdTime']:
+                master['logs_number'] = int(re.sub(r'\D', '', folder['name']))
+                master['logs_created_time'] = folder['createdTime']
+                master['logs_id'] = folder['id']
+
+    for length in range(3, 5 + 1):
+        for value in product(f"{ascii_lowercase}_", repeat=length):
+            username = ''.join(value)
+            if username.startswith('_') is False and re.search('__', username) is None \
+                    and username[:1].isdigit() is False:
+                if length in [3, 4]:
+                    username += 'bot'
+                if username.endswith('_') is False:
+                    master['max_users_count'] += 1
+
+    print(scripts)
+    print(master)
+    return keys, sheet, scripts
+
+
+# ========================================================================================================
+
+
+def google_update():
+    global worksheet, workers
+    while True:
+        try:
+            sleep(0)
+            try:
+                resources = worksheet.get('A1:Z50000')
+            except IndexError and Exception:
+                worksheet = gspread.service_account('google.json').open('master').worksheet('main')
+                resources = worksheet.get('A1:Z50000')
+            for worker in workers:
+                if worker['update'] == 1:
+                    row_index = len(resources) + 1
+                    for row in resources:
+                        if worker['PREFIX'] in row:
+                            row_index = resources.index(row) + 1
+                    values_indexes = range(0, len(google_keys))
+                    connection = heroku3.from_key(worker[f"HEROKU API {worker['server']}"])
+                    google_range = f'A{row_index}:{ascii_uppercase[values_indexes[-1]]}{row_index}'
+                    worksheet = gspread.service_account('google.json').open('master').worksheet('main')
+                    work_range = worksheet.range(google_range)
+                    for i in values_indexes:
+                        work_range[i].value = worker[google_keys[i]]
+                    worksheet.update_cells(work_range)
+                    for app in connection.apps():
+                        app.restart()
+                    worker['update'] = 0
+                    sleep(5)
+
+            resources.pop(0)
+            workers_count = 0
+            ended_workers_count = 0
+            for google_worker in resources:
+                if len(google_worker) == len(google_keys):
+                    workers_count += 1
+                    if google_worker[google_keys.index('PROGRESS')] == '🅰️':
+                        ended_workers_count += 1
+            if str(workers_count) != master['workers_count']:
+                for worker in workers:
+                    connection = heroku3.from_key(worker[f"HEROKU API {worker['server']}"])
+                    for app in connection.apps():
+                        app.restart()
+                for api in [master['another_api'], master['api']]:
+                    connection = heroku3.from_key(api)
+                    for app in connection.apps():
+                        config = app.config()
+                        config['workers_count'] = str(workers_count)
+            if workers_count == ended_workers_count:
+                drive_client = Drive('google.json')
+                logs_db = {'clear': [], 'used_count': 0}
+                objects.printer('Цикл проверок доступности юзеров пройден. '
+                                'Записываем список свободных username в google')
+
+                if len(drive_client.files(parents=master['logs_id'])) >= 400:
+                    folder = drive_client.create_folder(f"logs-{master['logs_number'] + 1}", master['main_id'])
+                    master['logs_created_time'] = folder['createdTime']
+                    master['logs_id'] = folder['id']
+                    master['logs_number'] += 1
+
+                for file in drive_client.files(parents=master['temp_id']):
+                    drive_client.download_file(file['id'], file['name'])
+                    local_file = open(file['name'], 'r')
+                    array = local_file.read().split(' ')
+                    if 'clear' in file['name']:
+                        logs_db['clear'].extend(array)
+                    else:
+                        logs_db['used_count'] += len(array)
+                    local_file.close()
+                    os.remove(file['name'])
+
+                if master['max_users_count'] == logs_db['used_count']:
+                    step = 50
+                    array = ' '.join(logs_db['clear'])
+                    chunks = [array[offset: offset + 50000] for offset in range(0, len(array), 50000)]
+                    spreadsheet = gspread.service_account('google.json').create('logs', master['logs_id'])
+                    worksheet = spreadsheet.sheet1
+                    worksheet.resize(rows=len(chunks), cols=1)
+                    spreadsheet.batch_update(objects.properties_json(worksheet.id, len(chunks), chunks[:step]))
+                    if len(chunks) > step:
+                        request_counter = 1
+                        for loop in range(step, len(chunks), step):
+                            row_begin = loop + 1
+                            row_end = row_begin + step
+                            if row_end > len(chunks):
+                                row_end = len(chunks)
+                            work_range = worksheet.range(f'A{row_begin}:A{row_end}')
+                            for row in range(row_begin, row_end + 1):
+                                if row <= len(chunks):
+                                    work_range[row - loop - 1].value = chunks[row - 1]
+                            worksheet.update_cells(work_range)
+                            request_counter += 1
+                            if request_counter == 50:
+                                request_counter = 0
+                                sleep(100)
+                    log_text = 'Успешно записана информация в telegram_usernames/' \
+                               f"logs-{master['logs_number']}/"
+                    objects.printer(log_text)
+                    for worker in workers:
+                        worker['update'] = 1
+                        worker['status'] = '✅'
+                    sleep(100)
+                else:
+                    log_text = objects.bold('Нарушена целостность массива\n(все workers закончили работу):\n') + \
+                        f"Необходимая длина массива проверок = {objects.code(master['max_users_count'])}\n" \
+                        f"Длина полученного массива = {objects.code(logs_db['used_count'])}"
+                    objects.printer(re.sub('<.*?>', '', log_text))
+                    ErrorAuth.send_dev_message(log_text, tag=None)
+                    sleep(10800)
+            sleep(300)
+        except IndexError and Exception:
+            ErrorAuth.thread_exec()
+
+
+def checking():
+    global worksheet
+    while True:
+        try:
+            sleep(20)
+        except IndexError and Exception:
+            ErrorAuth.thread_exec()
+
+
+master = {
+    'main_id': '',
+    'temp_id': '',
+    'logs_id': '',
+    'logs_number': 0,
+    'max_users_count': 0,
+    'logs_created_time': 0,
+    'api': os.environ.get('api'),
+    'another_api': os.environ.get('another_api'),
+    'workers_count': os.environ.get('workers_count')}
+
+t_me = 'https://t.me/'
+Auth = objects.AuthCentre(os.environ['DEV-TOKEN'])
+google_keys, worksheet, workers = variables_creation()
+ErrorAuth = objects.AuthCentre(os.environ['ERROR-TOKEN'])
+# ========================================================================================================
+
+
+def start(stamp):
+    for key in master:
+        if master[key] in ['', 0, None]:
+            break
+    else:
+        _thread.start_new_thread(google_update, ())
+        if os.environ.get('local') is None:
+            Auth.start_message(stamp)
+        checking()
+
+    ErrorAuth.start_message(stamp, f"\nОшибка с переменными окружения.\n{objects.bold('Бот выключен')}")
+
+
+start(objects.time_now())
