3a1fea125e87adad8f4dfe45fcf0246fefa46136
me <steve10.live@gmail.com>
2021-01-29 18:34:41 +0300

    Final vision of main file.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 import stat
 import shutil
 import objects
+from datetime import datetime
 from git.repo.base import Repo
 # ========================================================================================================
-stamp = objects.time_now()
+stamp = datetime.now().timestamp()
 
 
 def delete(action, name, exc):
@@ -13,14 +14,32 @@ def delete(action, name, exc):
     return action, name, exc
 
 
+def starting_print(timestamp):
+    text = 'Оболочка запущена за '
+    rounded = round(datetime.now().timestamp() - timestamp, 2)
+    if 0 < rounded < 1:
+        if len(str(rounded)) == 3:
+            rounded = f'{rounded}0'
+        text += f'{rounded} секунды'
+    else:
+        rounded = int(rounded)
+        text += f'{rounded} секунд'
+        if rounded < 10 or rounded > 20:
+            if str(rounded)[-1] in ['1']:
+                text += 'у'
+            elif str(rounded)[-1] in ['2', '3', '4']:
+                text += 'ы'
+    objects.printer(text)
+
+
 Repo.clone_from('https://github.com/evolvestin/Telegram-Username-Availability-Parser/', 'temp')
 for file_name in os.listdir('temp/master'):
     shutil.copy(f'temp/master/{file_name}', file_name)
 shutil.rmtree('temp', onerror=delete)
 # ========================================================================================================
-print(f'Запуск оболочки за {objects.time_now() - stamp} секунды')
+starting_print(stamp)
 
 
 if __name__ == '__main__':
     from master import start
-    start(stamp)
+    start(int(stamp))
