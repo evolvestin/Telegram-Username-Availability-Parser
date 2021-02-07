6f4e0261f56ac93232431248660f4cdda04af74c
me <steve10.live@gmail.com>
2021-02-08 02:59:25 +0300

    Drive class from GDrive.py moved to functions.py

--- worker/functions.py
+++ worker/functions.py
@@ -1,3 +1,4 @@
+import io
 import os
 import re
 import heroku3
@@ -6,14 +7,68 @@ import objects
 import _thread
 import requests
 from time import sleep
-from GDrive import Drive
 from copy import deepcopy
 import concurrent.futures
 from bs4 import BeautifulSoup
 from itertools import product
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 from string import ascii_lowercase, ascii_uppercase
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 # ========================================================================================================
 stamp1 = objects.time_now()
+standard_file_fields = 'files(id, name, parents, createdTime, modifiedTime)'
+
+
+class Drive:
+    def __init__(self, path):
+        scope = ['https://www.googleapis.com/auth/drive']
+        credentials = service_account.Credentials.from_service_account_file(path, scopes=scope)
+        self.client = build('drive', 'v3', credentials=credentials)
+
+    def update_file(self, file_id, file_path):
+        media_body = MediaFileUpload(file_path, resumable=True)
+        response = self.client.files().update(fileId=file_id, media_body=media_body).execute()
+        return response
+
+    def create_file(self, file_path, folder_id, same_file_name='True'):
+        if same_file_name == 'True':
+            same_file_name = re.sub('(.*)/', '', file_path)
+        media_body = MediaFileUpload(file_path, resumable=True)
+        file_metadata = {'name': same_file_name, 'parents': [folder_id]}
+        return self.client.files().create(body=file_metadata, media_body=media_body, fields='id').execute()
+
+    def download_file(self, file_id, file_path):
+        done = False
+        file = io.FileIO(file_path, 'wb')
+        downloader = MediaIoBaseDownload(file, self.client.files().get_media(fileId=file_id))
+        while done is False:
+            try:
+                status, done = downloader.next_chunk()
+            except IndexError and Exception:
+                done = False
+
+    def files(self, fields=standard_file_fields, only_folders=False, name_startswith=False, parents=False):
+        query = ''
+        response = []
+        if only_folders:
+            query = "mimeType='application/vnd.google-apps.folder'"
+        if name_startswith:
+            if query:
+                query += ' and '
+            query += f"name contains '{name_startswith}'"
+        if parents:
+            if query:
+                query += ' and '
+            query += f"'{parents}' in parents"
+        result = self.client.files().list(q=query, pageSize=1000, fields=fields).execute()
+        for file in result['files']:
+            for key in ['modifiedTime', 'createdTime']:
+                if file.get(key):
+                    stamp = re.sub(r'\..*?Z', '', file[key])
+                    file[key] = objects.stamper(stamp, '%Y-%m-%dT%H:%M:%S')
+            response.append(file)
+        return response
 
 
 def save_array_to_file(path, array):
